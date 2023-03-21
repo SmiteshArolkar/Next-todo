e3b13698907d2210e63693fd1e9b369857bd13c9
SMITESH AROLKAR <smitesharolkar99@gmail.com>
2023-03-21 13:48:36 +0530

    Animation

--- next-todo/components/Login.js
+++ next-todo/components/Login.js
@@ -15,8 +15,8 @@ export default function Login() {
           className="outline-none border-solid focus:border-cyan-500 border-white border-b-2 duration-300 text-black   p-1 w-full max-w-[65ch]"
           placeholder="Password"
         ></input>
-        <button className="w-full max-w-[65ch] border-solid p-1 py-2 border-white overflow-hidden border duration-300  relative after:absolute after:right-full
-         after:bg-white after:z-10 hover:text-black after:w-full after:h-full   hover:after:translate-x-full after:duration-300 transition-all after:-translate-y-6">
+        <button className="w-full max-w-[65ch] border-solid p-1 py-2 b overflow-hidden border duration-300  relative after:absolute after:right-full
+         after:bg-gradient-to-tr after:from-pink-800  after:to-red-600 after:via-purple-600 after:z-10 after:w-full after:h-full   hover:after:translate-x-full after:duration-300 transition-all after:-translate-y-6 md:after:-translate-y-7">
           <h2 className="relative z-20">SUBMIT</h2>
         </button>
       </div>
