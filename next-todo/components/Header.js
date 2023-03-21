0f66aed520434e5e5abacb13c7fb816cf9502154
SMITESH AROLKAR <smitesharolkar99@gmail.com>
2023-03-21 10:34:51 +0530

    Login Page

--- next-todo/components/Header.js
+++ next-todo/components/Header.js
@@ -2,8 +2,9 @@ import React from 'react';
 
 const Header = () => {
     return (
-        <div className='fixed top-0 w-full left-0 bg-green-300'>
-            Header
+        <div className='fixed top-0 w-full left-0 border-b  border-solid border-white sticky text-white bg-slate-900 text-3xl px-4 flex items-center justify-between p-4'>
+            <h1 className='text-3xl sm:text-6xl select-none'>TODO LIST</h1>
+            <i className="fa-solid fa-user  sm:text-4xl duration-300 hover:opacity-40 cursor-pointer"></i>
         </div>
     );
 }
--- next-todo/components/Layout.js
+++ next-todo/components/Layout.js
@@ -1,3 +1,4 @@
+import Footer from "./Footer"
 import Header from "./Header"
 
 export default function Layout(props)
@@ -6,9 +7,10 @@ export default function Layout(props)
     return (
         <div className="flex flex-col min-h-screen relative bg-slate-900">
             <Header></Header>
-            <main className="flex-1  bg-yellow-50">
+            <main className="flex-1 flex flex-col p-4">
             {children}
             </main>
+            <Footer></Footer>
         </div>
     )
 }
